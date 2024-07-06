2b66c0737ff70d698ae7c1446a4e9df0c648ecdb
William L. Anderson <43255288+01874@users.noreply.github.com>
2024-07-05 21:30:29 -0500

    Add dataclass to store model hyperparams

--- model.py
+++ model.py
@@ -13,3 +13,20 @@ from typing import Tuple, List, Optional, Dict, Callable
 from jaxtyping import Float, Int
 from torch.utils.data import DataLoader
 import wandb
+
+device = t.device("cuda" if t.cuda.is_available() else "cpu")
+#%%
+# dataclass to store model hyperparams
+@dataclass
+class Config:
+    d_model: int = 256
+    debug: bool = True
+    layer_norm_eps: float = 1e-5
+    d_vocab: int = 2
+    init_range: float = 0.02
+    n_ctx: int = 2048
+    d_head: int = 32
+    d_mlp: int = 768
+    n_heads: int = 8
+    n_layers: int = 8
+# %%
